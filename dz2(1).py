92a06b65a727190d2146cf418fd6de5e9d2a7818
matroarh <finn.85@mail.ru>
2020-01-29 19:23:04 +0300

    del

--- dz2(1).py
+++
@@ -1,3 +0,0 @@
-my_list = [65, 'text', 5.2]
-a = int(input('Введите номер элемента: '))
-print (type(my_list [a - 1]))
--- dz2(4).py
+++
@@ -1,5 +0,0 @@
-text = input('Введите строку из нескольких слов: ')
-text = list((text).split())
-
-for i, n in enumerate(text):
-    print(i, n[:10])
